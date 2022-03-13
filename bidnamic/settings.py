8c9571b7c15f962f53cad899376833c8c78ad3d7
Zues D'ghost <zuesghost1@gmail.com>
2022-03-13 06:24:44 +0100

    Update settings.py

--- bidnamic/settings.py
+++ bidnamic/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-zgb1%5um=!w)yv$f7#j$ro=!)cjw5fw@x%ja)3m3ygu+sobioh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-csv.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
